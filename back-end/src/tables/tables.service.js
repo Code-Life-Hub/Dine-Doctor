9f54510060f7908de3f0efc338bb02256eb86017
David Wright.Developer <dwright9157@icloud.com>
2023-10-24 11:28:56 -0500

    Feature Update

--- back-end/src/tables/tables.service.js
+++ back-end/src/tables/tables.service.js
@@ -3,31 +3,36 @@
 const knex = require("../db/connection");
 
 function create(newTable) {
-	return knex("tables")
-		.insert(newTable)
-		.returning("*")
-		.then((createdRecord) => createdRecord[0]);
+  return knex("tables")
+    .insert(newTable)
+    .returning("*")
+    .then((createdRecord) => createdRecord[0]);
 }
 
 function read(table_id) {
-	return knex("tables").select("*").where({ table_id }).first();
+  return knex("tables").select("*").where({ table_id }).first();
 }
 
 function update(updatedTable) {
-	return knex("tables")
-		.select("*")
-		.where({ table_id: updatedTable.table_id })
-		.update(updatedTable, "*")
-		.then((updatedRecords) => updatedRecords[0]);
+  return knex("tables")
+    .select("*")
+    .where({ table_id: updatedTable.table_id })
+    .update(updatedTable, "*")
+    .then((updatedRecords) => updatedRecords[0]);
+}
+
+function destroy(table_id) {
+  return knex("tables").where({ table_id }).del();
 }
 
 function list() {
-	return knex("tables").select("*").orderBy("table_name");
+  return knex("tables").select("*").orderBy("table_name");
 }
 
 module.exports = {
-	create,
-	read,
-	update,
-	list,
+  create,
+  read,
+  update,
+  destroy,
+  list,
 };
